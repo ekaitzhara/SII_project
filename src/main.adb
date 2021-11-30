5894df84f9a90ad6cf115ecd54d59c64c9497f29
EnaraSC <enara.sc19@gmail.com>
2021-11-30 10:06:58 +0100

    Enara: Menu cambio

--- src/main.adb
+++ src/main.adb
@@ -1,9 +1,8 @@
 with Ada.Text_IO;   use Ada.Text_IO;
-
+with Utilidades; use Utilidades;
 procedure Main is
 CMenu: Character;
 begin
-
 
   Loop
    CMenu:= Fc_Menu;
@@ -24,8 +23,6 @@ begin
          exit;
       when others =>
          null;
-
-
       end case;
    end loop;
 
