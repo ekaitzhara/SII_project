acc475e65c5ca51d57a7d580c76f7a57f5341fe2
EnaraSC <enara.sc19@gmail.com>
2021-11-30 09:58:10 +0100

    Enara: Menu

--- src/main.adb
+++ src/main.adb
@@ -1,17 +1,30 @@
 with Ada.Text_IO;   use Ada.Text_IO;
 
 procedure Main is
-
+CMenu: Character;
 begin
-   --  Insert code here.
-   -- Comentario Ekaitz
 
-	--fduyhfduyfd
+  Loop
+   CMenu:= Fc_Menu;
+   case CMenu is
+      when '1' =>
+         Put("1");
+      when '2' =>
+         Put("2");
+      when '3' =>
+         Put("3");
+      when '4' =>
+         Put("4");
+      when '5' =>
+         Put("5");
+      when '6' =>
+         Put("6");
+      when '0' =>
+         exit;
+      when others =>
+         null;
 
-
-   --Ekaitz, gracias por este cursillo, tkm
-   --holi
-
-   Put_Line("Hello world!");
+      end case;
+   end loop;
 
 end Main;
--- src/paquetes/utilidades.ads
+++ src/paquetes/utilidades.ads
@@ -1,5 +1,8 @@
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+
 package utilidades is
 
-   
+   function Fc_Menu return Character;
 
 end utilidades;
