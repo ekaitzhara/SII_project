886e2c7505e7461d37ed5cfcc4b48a57aeef861f
ekaitzhara <ekaitzhara16@gmail.com>
2021-12-13 09:11:20 +0100

    Ekaitz: aÃ±adido manejo arrays float

---
+++ src/paquetes/manejo_array_float.ads
@@ -0,0 +1,14 @@
+with tipos;  use tipos;
+
+package manejo_array_float is
+   
+   procedure getElemArray(lista:in out listaElementos; numberOfElem: in Integer);
+
+   procedure mostrarArray(lista: listaElementos);
+   
+   procedure mediaTemperatura(lista: listaElementos; cont: Integer);
+   
+   procedure umbralTemperatura(lista: listaElementos; cont: Integer);
+   
+
+end manejo_array_float;
