78c77e6b08188ca2bb0fe3a4943f4c95b92aff23
samupousa <samuelpousinha@gmail.com>
2021-12-22 13:18:46 +0100

    ada calendar

--- src/paquetes/manejo_array_float.adb
+++ src/paquetes/manejo_array_float.adb
@@ -1,6 +1,7 @@
 with Ada.Text_IO;   use Ada.Text_IO;
 with Ada.Integer_Text_IO;   use Ada.Integer_Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+with Ada.Calendar;            use Ada.Calendar;
 
 package body manejo_array_float is
    
